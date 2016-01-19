4325615ab933f001433a677f7af3527b3c02a4c6
chi777 <baloneymath@gmail.com>
2016-01-19 15:00:33 +0800

    still fighting with simulate

--- src/cir/cirSim.cpp
+++ src/cir/cirSim.cpp
@@ -51,16 +51,16 @@ void
 CirMgr::fileSim(ifstream& patternFile)
 {
   string line;
-  size_t inputs[_params[1]];
+  size_t input;
   size_t nPatterns = 0;
-  for (size_t i = 0; i < _params[1]; ++i)
-    inputs[i] = 0;
 
+  HashMap<SimKey, IDList> FECGrps(0);
+  FECGrps.forceInsert(0x0, _dfsList);
   while (getline(patternFile, line)) {
     // check the length of patterns
     if (line.size() != _params[1]) {
       if (!line.empty()) {
-        cerr << "\nError: Pattern(" << line << ") length(" << line.size()
+        cerr << "Error: Pattern(" << line << ") length(" << line.size()
           << ") does not match the number of inputs(" << _params[1]
           << ") in a circuit!!\n";
       }
@@ -69,19 +69,19 @@ CirMgr::fileSim(ifstream& patternFile)
     // check if the patterns contain some trash (ex. 00102001x300)
     size_t pos = find_first_not_of("01");
     if (pos != string::npos) {
-      cerr << "\nError: Pattern(" << line << ") contains a non-0/1 character(\'"
+      cerr << "Error: Pattern(" << line << ") contains a non-0/1 character(\'"
         << line[pos] << "\').\n";
       break;
     }
     // convert inputs patterns
     for (size_t i = 0; i < _params[1]; ++i) {
-      inputs[i] = (size_t)(line[1] == '1');
-      inputs[i] <<= 1;
+      input = input | (size_t)(line[i] == '1');
+      if (i != _params[1]-1) input <<= 1;
     }
     ++nPatterns;
 
     // start to simulate
-
+    FECGrps = simulate(FECGrps, input);
   }
 
 
@@ -92,15 +92,38 @@ CirMgr::fileSim(ifstream& patternFile)
 /*************************************************/
 
 HashMap<SimKey, IDList>
-CirMgr::simulate(HashMap<SimKey, IDList> FECGrps)
+CirMgr::simulate(HashMap<SimKey, IDList> FECGrps, size_t input)
 {
+  // set simValue
+  size_t simValue[_params[0] + 1];
+  simValue[0] = 0; // const0
+  for (size_t i = 0; i < _dfsList.size(); ++i) {
+    if (_dfsList[i]->getType() == PI_GATE) {
+      size_t value  = (size_t)( (input>>(_params[1]-i)) & 1);
+      simValue[_dfsList[i]->getId()] = value;
+    }
+    else if (_dfsList[i]->getType() == AIG_GATE) {
+      IDList& fanin = _dfsList[i]->_fanin;
+      CirGate* in[2];
+      in[0] = (CirGate*)(fanin[0] & ~(size_t)(0x1));
+      in[1] = (CirGate*)(fanin[1] & ~(size_t)(0x1));
+      size_t value = simValue[in[0]->getId()] & simValue[in[1]->getId()];
+      simValue[_dfsList[i]->getId()] = value;
+    }
+    else if (_dfsList[i]->getType() == PO_GATE) {
+      CirGate* in = (CirGate*)(_dfsList[i]->_fanin[0] & ~(size_t)(0x1));
+      size_t value = simValue[in->getId()];
+      simValue[_dfsList[i]->getId()] = value;
+    }
+  }
+  // check FECGrps
   HashMap<SimKey, IDList> target;
   for (FECGrps::iterator it = FECGrps.begin(); it != FECGrps.end(); ++it) {
     HashMap<SimKey, IDList> newFECGrps(0);
     for (size_t i = 0; i < it.second.size(); ++i) {
       IDList temp;
       CirGate* gate = (CirGate*)(it.second[i] & ~(size_t)(0x1));
-      size_t key = gate->_fanin[0] & gate_fanin[1];
+      size_t key = simValue[gate->getId()];
       if (newFECGrps.check(key, temp)) {
         temp.push_back((size_t)gate);
       }
